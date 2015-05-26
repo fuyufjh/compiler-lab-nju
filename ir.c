3c8472f812ddb4970c1c8641f0cf1c2eaef33acb
Klose Fu <fuyufjh@163.com>
2015-05-26 17:34:52 +0800

    structure for IR finished

---
+++ ir.c
@@ -0,0 +1,122 @@
+#include "ir.h"
+#include <stdio.h>
+
+void add_ir_code(struct ir_code *code) {
+    struct ir_node *node = new(struct ir_node);
+    node->code = code;
+    node->next = NULL;
+    if (ir_list_head == NULL) {
+        ir_list_head = ir_list_tail = node;
+    } else {
+        ir_list_tail->next = node;
+        ir_list_tail = node;
+    }
+}
+
+static FILE *out;
+
+static char *get_operand_str(struct ir_operand *op) {
+    // DO NOT free the return value!
+    static char buf[32];
+    switch (op->kind) {
+    case OP_VARIABLE:
+        sprintf(buf, "v%d", op->no);
+        break;
+    case OP_IMMEDIATE:
+        sprintf(buf, "#%d", op->val_int);
+        break;
+    case OP_TEMP_VAR:
+        sprintf(buf, "t%d", op->no);
+        break;
+    case OP_LABEL:
+        sprintf(buf, "label%d", op->no);
+        break;
+    case OP_FUNCTION:
+        sprintf(buf, "%s", op->name);
+        break;
+    }
+    return buf;
+}
+
+char* relop_str[] = {
+    "==", "!=", ">", "<", ">=", "<="
+};
+
+static void print_ir_code(struct ir_code *code) {
+#define OP(op) get_operand_str(code->op)
+    switch (code->kind) {
+    case IR_LABEL:
+        fprintf(out, "LABEL %s :", OP(op));
+        break;
+    case IR_FUNCTION:
+        fprintf(out, "FUNCTION %s :", OP(op));
+        break;
+    case IR_ASSIGN:
+        fprintf(out, "%s := %s", OP(dst), OP(src));
+        break;
+    case IR_ADD:
+        fprintf(out, "%s := %s + %s", OP(dst), OP(src1), OP(src2));
+        break;
+    case IR_SUB:
+        fprintf(out, "%s := %s - %s", OP(dst), OP(src1), OP(src2));
+        break;
+    case IR_MUL:
+        fprintf(out, "%s := %s * %s", OP(dst), OP(src1), OP(src2));
+        break;
+    case IR_DIV:
+        fprintf(out, "%s := %s / %s", OP(dst), OP(src1), OP(src2));
+        break;
+    case IR_GET_ADDRESS:
+        fprintf(out, "%s := &%s", OP(dst), OP(src));
+        break;
+    case IR_GET_VALUE:
+        fprintf(out, "%s := *%s", OP(dst), OP(src));
+        break;
+    case IR_ASSIGN_TO_ADDRESS:
+        fprintf(out, "*%s := %s", OP(dst), OP(src));
+        break;
+    case IR_GOTO:
+        fprintf(out, "GOTO %s", OP(op));
+        break;
+    case IR_IF_GOTO:
+        fprintf(out, "IF %s %s %s GOTO %s", OP(src1), relop_str[code->relop], OP(src2), OP(dst));
+        break;
+    case IR_RETURN:
+        fprintf(out, "RETURN %s", OP(op));
+        break;
+    case IR_DEC:
+        fprintf(out, "DEC %s %d", OP(op), code->size);
+        break;
+    case IR_ARG:
+        fprintf(out, "ARG %s", OP(op));
+        break;
+    case IR_CALL:
+        fprintf(out, "%s := CALL %s", OP(ret), OP(func));
+        break;
+    case IR_PARAM:
+        fprintf(out, "PARAM %s", OP(op));
+        break;
+    case IR_READ:
+        fprintf(out, "READ %s", OP(op));
+        break;
+    case IR_WRITE:
+        fprintf(out, "WRITE %s", OP(op));
+        break;
+    default:
+        exit(-1);
+    }
+    fprintf(out, "\n");
+}
+
+void print_ir_list(FILE *fp) {
+#ifndef IR_DEBUG
+    out = fp;
+#else
+    out = stdout;
+#endif
+    struct ir_node *p = ir_list_head;
+    while (p != NULL) {
+        print_ir_code(p->code);
+        p = p->next;
+    }
+}
---
+++ ir.h
@@ -0,0 +1,78 @@
+#ifndef _IR_H
+#define _IR_H
+
+#include "common.h"
+
+struct ir_operand {
+    enum op_type {
+        OP_VARIABLE,
+        OP_IMMEDIATE,
+        OP_TEMP_VAR,
+        OP_LABEL,
+        OP_FUNCTION
+    } kind;
+    union {
+        int no;
+        int val_int;
+        char *name;
+    };
+};
+
+struct ir_code {
+    enum ir_type {
+        IR_LABEL,
+        IR_FUNCTION,
+        IR_ASSIGN,
+        IR_ADD,
+        IR_SUB,
+        IR_MUL,
+        IR_DIV,
+        IR_GET_ADDRESS,
+        IR_GET_VALUE,
+        IR_ASSIGN_TO_ADDRESS,
+        IR_GOTO,
+        IR_IF_GOTO,
+        IR_RETURN,
+        IR_DEC,
+        IR_ARG,
+        IR_CALL,
+        IR_PARAM,
+        IR_READ,
+        IR_WRITE,
+    } kind;
+    union {
+        struct { // 3-operand
+            struct ir_operand *dst, *src1, *src2;
+        };
+        struct { // 2-operand
+            struct ir_operand *_dst, *src;
+        };
+        struct { // 1-operand
+            struct ir_operand *op;
+        };
+        struct { // for RETURN only
+            struct ir_operand *ret, *func;
+        };
+    };
+    union {
+        enum relop_type {
+            RELOP_EQ,
+            RELOP_NEQ,
+            RELOP_G,
+            RELOP_L,
+            RELOP_GE,
+            RELOP_LE,
+        } relop;
+        int size;
+    };
+};
+
+struct ir_node {
+    struct ir_code *code;
+    struct ir_node *next;
+} *ir_list_head, *ir_list_tail;
+
+void add_ir_code(struct ir_code *code);
+void print_ir_list();
+
+#endif
