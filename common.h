aea5cdd63c5847c95f9829793ffcbd0e73812e00
Klose Fu <fuyufjh@163.com>
2015-05-27 01:14:14 +0800

    new powerful new()!

--- common.h
+++ common.h
@@ -8,10 +8,15 @@
 #include <stdio.h>
 
 // General macro define
-#define new(type) ((type*) malloc(sizeof(type)))
 #define bool int
 #define true 1
 #define false 0
+
+// WOW! I invented this! DIAO BAO LE!
+#define new(type, ...) ({\
+        type* t = (type*)malloc(sizeof(type));\
+        *t = (type){__VA_ARGS__};\
+        t; })
 
 // Global flags
 bool flag_print_ast;
